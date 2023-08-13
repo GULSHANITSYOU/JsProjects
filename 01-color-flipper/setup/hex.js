e794ccd66aabeebf7d225ba9df746fc90654c69d
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2023-08-13 11:26:19 +0530

    Add random color flipper functionality Added the functionality to generate random colors in both simple and hex formats to the color flipper application. Users can now click the Generate button to get a new random color displayed on the screen. The app utilizes JavaScript to generate random RGB values and convert them to hex codes. Additionally, a button is included to copy the generated hex color to the clipboard for easy use. This enhancement enhances user engagement by providing an interactive and visually appealing feature to the application.

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,23 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn = document.getElementById('btn') ; 
+const color = document.querySelector('.color'); 
+
+const randomNumber = ()=> {
+    return Math.floor(Math.random() * hex.length) ; 
+}
+
+btn.addEventListener('click' , ()=>{
+    let hexCode = "#" ; 
+
+    for(let i = 0 ; i<6 ; i++){
+        hexCode += hex[randomNumber()]; 
+    }
+
+    document.body.style.backgroundColor = hexCode; 
+    color.textContent = hexCode ; 
+    console.log(hexCode)
+
+
+})
