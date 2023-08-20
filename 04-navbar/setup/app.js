09ff71976b3be3479986daacc3e69f9d4135641d
GUSLHAN KUMAR <gulshanitsyou@gmail.com>
2023-08-21 00:03:57 +0530

    nav bar project

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,21 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+// Select the toggle button element
+const toggleBtn = document.querySelector('.nav-toggle');
+
+// Select the links container element
+const links = document.querySelector('.links');
+
+// Add a click event listener to the links container
+toggleBtn.addEventListener('click', () => {
+    // Check if the links container has the class "show-links"
+    if (links.classList.contains("show-links")) {
+        // If it has the class, remove it to hide the links
+        links.classList.remove('show-links');
+    } else {
+        // If it doesn't have the class, add it to show the links
+        links.classList.add("show-links");
+    }
+});
